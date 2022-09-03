89f0dec25ec6f4e52ebacbe8f680323ba50eb00b
jieHFUT <3492779706@qq.com>
2022-09-03 16:41:24 +0800

    review

--- review_java/src/review_java_01/MyDoubleLinkedLIst.java
+++ review_java/src/review_java_01/MyDoubleLinkedLIst.java
@@ -1,5 +1,8 @@
 package review_java_01;
 
+
+import java.util.Collection;
+
 /**
  * Created with IntelliJ IDEA.
  * Description:
@@ -7,6 +10,220 @@ package review_java_01;
  * Date: 2022-09-02
  * Time: 8:42
  */
-public class MyDoubleLinkedLIst {
+public class MyDoubleLinkedLIst<E> {
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+
+    transient int size;
+
+    transient Node<E> first;
+
+    transient Node<E> last;
+
+    public MyDoubleLinkedLIst(){
+
+    }
+
+    public MyDoubleLinkedLIst(Collection<? extends E> e){
+        this();
+        this.addAll(e);
+    }
+
+
+    @Override
+    public String toString() {
+       Node<E> eNode = this.first;
+       if (eNode == null) return null;
+       StringBuilder stringBuilder = new StringBuilder("[");
+       while (eNode.next != null){
+           stringBuilder.append(eNode.item + ",");
+           eNode = eNode.next;
+       }
+       stringBuilder.append(eNode.item + "]");
+       return stringBuilder.toString();
+    }
+
+    public int size(){
+        return this.size;
+    }
+
+
+    public void addFirst(E e){
+        Node<E> eNode = new Node<>(null,e,null);
+        if (this.first == null){
+            this.first = eNode;
+            this.last = eNode;
+            this.size++;
+            return;
+        }
+        eNode.next = this.first;
+        this.first.prev = eNode;
+        this.first = eNode;
+        this.size++;
+        return;
+    }
+
+
+    public void addLast(E e){
+        Node<E> eNode = new Node<>(null,e,null);
+        if (this.first == null){
+            this.first = eNode;
+            this.last = eNode;
+            this.size++;
+            return;
+        }
+        this.last.next = eNode;
+        eNode.prev = this.last;
+        this.size++;
+        return;
+    }
+
+
+    public boolean addIndex(int index, E e){
+
+        if (index < 0 || index > this.size()){
+            throw new IllegalArgumentException("输入的范围越界！");
+        }
+        Node<E> eNode = new Node<>(null,e,null);
+        if (this.first == null){
+            this.first = eNode;
+            this.last = eNode;
+            this.size++;
+            return true;
+        }
+        Node<E> currentFirst = this.first;
+        if (index == this.size()){
+            this.last.next = eNode;
+            eNode.prev = this.last;
+            this.last = eNode;
+            this.size++;
+            return true;
+        }
+        while (index > 0){
+            currentFirst = currentFirst.next;
+            index--;
+        }
+        eNode.next = currentFirst;
+        eNode.prev = currentFirst.prev;
+        currentFirst.prev.next = eNode;
+        currentFirst.prev = eNode;
+        this.size++;
+        return true;
+    }
+
+
+    public void addAll(Collection<? extends E> e){
+
+    }
+
+    public boolean contains(E e){
+        Node<E> eNode = this.first;
+        while (eNode != null){
+            if (eNode.item.equals(e)){
+                return true;
+            }
+            eNode = eNode.next;
+        }
+        return false;
+    }
+
+
+
+
+    public boolean remove(E e){
+        if (this.first == null){
+            throw new IllegalArgumentException("链表为空！无法删除！");
+        }
+
+        if (this.first.equals(this.last) && this.first.item.equals(e)){
+            this.first = null;
+            this.last = null;
+            this.size--;
+            return true;
+        }
+
+        if (this.first.item.equals(e)){
+            this.first = this.first.next;
+            if (this.first != null){
+                this.first.prev = null;
+            }
+            this.size--;
+            return true;
+        }
+        if (this.last.item.equals(e)){
+            this.last.prev.next = null;
+            this.last.prev = null;
+            this.size--;
+            return true;
+        }
+
+        Node<E> eNode = this.first;
+        while (eNode != null){
+            if (eNode.item.equals(e)){
+                eNode.prev.next = eNode.next;
+                eNode.next.prev = eNode.prev;
+                this.size--;
+                return true;
+            }
+            eNode = eNode.next;
+        }
+        throw new IllegalArgumentException("链表中没有你要删除的元素！");
+    }
+
+
+
+    public void removeAllKey(E e){
+        if (this.first == null) {
+            throw new IllegalArgumentException("该链表为空，无法进行删除！");
+        }
+        while (this.first != null && this.first.item.equals(e)){
+            this.first = this.first.next;
+            if (this.first != null){
+                this.first.prev = null;
+            }
+            this.size--;
+        }
+        if (this.first == null) return ;
+        Node<E> current = this.first;
+        Node<E> preCur = this.first;
+        Node<E> nextCur = this.first.next;
+        while (nextCur != null){
+            if (nextCur.item.equals(e)){
+                current = nextCur;
+                nextCur = nextCur.next;
+                if (nextCur != null){
+                    nextCur.prev = preCur;
+                }
+                preCur.next = nextCur;
+                current.prev = null;
+                current.next = null;
+                current = preCur;
+                this.size--;
+            }else {
+                current = nextCur;
+                nextCur = nextCur.next;
+                preCur = current;
+            }
+        }
+        return ;
+    }
+
+
+
+
+
+
+
 
 }
--- review_java/src/review_java_01/TestMyDoubleLinkedList.java
+++ review_java/src/review_java_01/TestMyDoubleLinkedList.java
@@ -1,5 +1,9 @@
 package review_java_01;
 
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 /**
  * Created with IntelliJ IDEA.
  * Description:
@@ -8,5 +12,67 @@ package review_java_01;
  * Time: 8:42
  */
 public class TestMyDoubleLinkedList {
+    public static void main(String[] args) {
+        MyDoubleLinkedLIst<Integer> myDoubleLinkedLIst = new MyDoubleLinkedLIst<>();
+        myDoubleLinkedLIst.addFirst(10);
+
+        myDoubleLinkedLIst.remove(10);
+        System.out.println(myDoubleLinkedLIst);
+
+        myDoubleLinkedLIst.addFirst(11);
+        myDoubleLinkedLIst.addFirst(12);
+        myDoubleLinkedLIst.addFirst(13);
+        myDoubleLinkedLIst.addFirst(14);
+
+        System.out.println(myDoubleLinkedLIst);
+        System.out.println(myDoubleLinkedLIst.size());
+
+        myDoubleLinkedLIst.addIndex(2,99);
+        System.out.println(myDoubleLinkedLIst);
+
+        System.out.println(myDoubleLinkedLIst.contains(99));
+        System.out.println(myDoubleLinkedLIst.contains(199));
+
+        myDoubleLinkedLIst.remove(99);
+        //myDoubleLinkedLIst.remove(999);
+        System.out.println(myDoubleLinkedLIst);
+
+        myDoubleLinkedLIst.addFirst(11);
+        myDoubleLinkedLIst.addFirst(11);
+        myDoubleLinkedLIst.addFirst(11);
+        myDoubleLinkedLIst.addIndex(3,11);
+        myDoubleLinkedLIst.addLast(11);
+        myDoubleLinkedLIst.addIndex(5,11);
+        System.out.println(myDoubleLinkedLIst);
+        myDoubleLinkedLIst.removeAllKey(11);
+        System.out.println(myDoubleLinkedLIst);
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        //遍历
+        linkedList.add(10);
+        linkedList.add(20);
+        linkedList.add(30);
+        linkedList.add(40);
+
+        for (int x:
+             linkedList) {
+            System.out.print(x + ",");
+        }
+        System.out.println();
+
+
+        ListIterator<Integer> integerListIterator1 = linkedList.listIterator();
+        while (integerListIterator1.hasNext()){
+            System.out.print(integerListIterator1.next() + ",");
+        }
+        System.out.println();
+
+        ListIterator<Integer> integerListIterator2 = linkedList.listIterator(linkedList.size());
+        while (integerListIterator2.hasPrevious()){
+            System.out.print(integerListIterator2.previous() + ",");
+        }
+        System.out.println();
+
+    }
 
 }
