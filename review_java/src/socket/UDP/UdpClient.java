532cef9da6403ea2ade0b17078c6af55b0a3c72f
JIE <3492779706@qq.com>
2022-10-22 23:04:05 +0800

    review

---
+++ review_java/src/socket/UDP/UdpClient.java
@@ -0,0 +1,29 @@
+package socket.UDP;
+
+import java.io.IOException;
+import java.net.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: JIE
+ * Date: 2022-10-21
+ * Time: 20:40
+ */
+public class UdpClient {
+
+    // 服务器socket的地址，包含域名或者IP，端口号
+    private static final SocketAddress ADDRESS = new InetSocketAddress("localhost", 8888);
+
+    public static void main(String[] args) throws IOException {
+        // 创建客户端DatagramSocket， 开启随机端口就行，可以发送及接收UDP数据报
+        DatagramSocket socket = new DatagramSocket();
+
+        // 准备要发送的数据
+        byte[] bytes = "hello socket".getBytes();
+        DatagramPacket packet = new DatagramPacket(bytes, bytes.length, ADDRESS);
+
+        // 发送UDP数据报
+        socket.send(packet);
+    }
+}
---
+++ review_java/src/socket/UDP/UdpServer.java
@@ -0,0 +1,51 @@
+package socket.UDP;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: JIE
+ * Date: 2022-10-21
+ * Time: 20:23
+ */
+public class UdpServer {
+
+    // 服务器socket要绑定的端口号
+    private static final int PORT = 8888;
+
+    public static void main(String[] args) throws IOException {
+        // 1、创建服务端 DatagramSocket, 指定端口，可以发送及接收UDP数据报
+        DatagramSocket socket = new DatagramSocket(PORT);
+        // 不停的去接收客户端的udp数据报
+        while (true) {
+            // 2、创建数据报，用于接收客户端发送的数据
+            byte[] bytes = new byte[1024];
+            DatagramPacket packet = new DatagramPacket(bytes, bytes.length);
+            System.out.println("----------------------------------------");
+            System.out.println("等待接收到客户端的数据报");
+            // 3、等待接收客户端发送的UDP数据报，该方法在接收到数据报之前一直阻塞，接收到数据报以后，DatagramPacket对象，包含数据（bytes）, 客户端IP，端口号
+            socket.receive(packet);
+            System.out.printf("客户端   IP：%s%n",packet.getAddress().getHostAddress());
+            System.out.printf("客户端端口号：%s%n",packet.getPort());
+            System.out.printf("客户端发送的原生数据为：%s%n", Arrays.toString(packet.getData()));
+            System.out.printf("客户端发送的文本数据为：%s%n", new String(packet.getData()));
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+}
